d730f4ed57c6bc278b77a073b3f180d4bfe44270
Carlos Eduardo Hernandez Aguilar <carloseduardohernandezaguilar@macbook-pro-de-carlos.local>
2018-05-06 21:59:33 -0500

    Initial Commit

---
+++ AppTest3Tests/AppTest3Tests.swift
@@ -0,0 +1,36 @@
+//
+//  AppTest3Tests.swift
+//  AppTest3Tests
+//
+//  Created by Carlos Eduardo Hernandez Aguilar on 06/05/18.
+//  Copyright © 2018 Carlos Eduardo Hernandez Aguilar. All rights reserved.
+//
+
+import XCTest
+@testable import AppTest3
+
+class AppTest3Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
