85567ee9cfa2c67771a5116ea168e6d39bdff882
Carlos Eduardo Hernandez Aguilar <carloseduardohernandezaguilar@MacBook-Pro-de-Carlos.local>
2018-05-07 20:44:03 -0500

    Terminado

--- AppTest3/ViewController.swift
+++ AppTest3/ViewController.swift
@@ -9,17 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print("\(self.appTest3(start: 1923, end: 2018))")
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func appTest3(start: Int,end :Int) -> String {
+        
+        var years = start
+        
+        var yearsArray = Array<Int>()
+        
+        for _ in start...end {
+            
+            if(self.isLeapYear(years)){
+                yearsArray.append(years)
+            }
+            years = years + 1
+        }
+        var leapYears = ""
+        print("Años bisiestos: \(yearsArray)")
+        for y in yearsArray{
+            leapYears = leapYears + "\(y)@"
+        }
+        
+        return leapYears
+    }
+    
+    func isLeapYear(_ year: Int) -> Bool {
+        
+        let isLeapYear = ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0))
+        
+        return isLeapYear
+    }
 }
 
